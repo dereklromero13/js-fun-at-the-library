1f10ff6c8b3fe4d6c143e59fecc8b7b8f0459846
Derek Romero <dereklromero13@gmail.com>
2020-05-07 15:49:07 -0600

    Add createTitle functionality

--- src/book.js
+++ src/book.js
@@ -1,11 +1,13 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
+  buildMainCharacter: buildMainCharacter,
+  //saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
   // writeBook: writeBook,
   // editBook: editBook
