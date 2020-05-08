40a9600bff5dfe7994d426f4a30f0a7c25ed2c05
Derek Romero <dereklromero13@gmail.com>
2020-05-07 20:40:07 -0600

    Add shelfBook and unshefBook functionality

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,20 @@
+function shelfBook(scifiBook, scifiShelf) {
+  if (scifiShelf.length < 3) {
+  scifiShelf.unshift(scifiBook)
+  }
+};
 
+function unshelfBook(book, scifiShelf) {
+  for (var i = 0; i < scifiShelf.length; i++) {
+    if (scifiShelf[i].title === "Dune") {
+      scifiShelf.splice(i, 1)
+    }
+  }
+};
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
   // searchShelf: searchShelf
 };
