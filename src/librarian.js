8d37a6b516fbed32b8125b02b3cc60d239d250d3
Derek Romero <dereklromero13@gmail.com>
2020-05-10 20:43:40 -0600

    Add Librarain class functionality

--- src/librarian.js
+++ src/librarian.js
@@ -1,8 +1,26 @@
 class Librarian {
-  constructor() {
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
 
-  }
-  
-}
+  greetPatron(patron, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${patron}!`;
+    } else {
+      return `Hello, ${patron}!`;
+    }
+  };
+
+  findBook(book) {
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (book === this.library.shelves.fantasy[i].title) {
+        this.library.shelves.fantasy.splice(i, 1)
+        return `Yes, we have ${book}`;
+      }
+    }
+    return `Sorry, we do not have ${book}`;
+  };
+};
 
 module.exports = Librarian;
